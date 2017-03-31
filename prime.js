9c19ebbbc7a0f3fd9e184bd60a08de72082e2649
Adeyemi Ifedayo <adeyemiife@ymail.com>
2017-03-31 08:08:31 -0700

    Add files via upload

---
+++ prime.js
@@ -0,0 +1,29 @@
+<script>
+var i, j;
+
+var num = parseInt(prompt("Enter number to find it's prime number"));
+
+var count;
+
+for(i = 1;i <= num;i++){
+
+	count = 0;
+
+	for(j = 1;j <= i;j++){
+
+		if(i % j == 0){
+
+			count = count + 1;
+
+        	}
+
+    	}
+
+	if(count == 2){
+
+		document.write(i + " is a prime number<br>");
+
+    }
+
+}
+</script>
